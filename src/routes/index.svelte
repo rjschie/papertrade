bf650d4e6f3dd76a931cc1631c2707ee3cab0e22
Ryan Schie <rjschie@gmail.com>
2021-10-15 11:17:15 -0700

    feat: add main page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,14 @@
-<h1>Paper Trade</h1>
-<p>A simulated paper trading app for Binance exchange</p>
+<main class="flex flex-col h-full items-center justify-center">
+  <h1 class="mb-3 text-b300 font-bold font-display text-6xl">Paper Trade</h1>
+  <p class="mb-10 text-n700">
+    A simulated paper trading app using Binance live market data
+  </p>
+  <a href="/trade" class="px-3 py-2 rounded-lg bg-b500 text-n0">Get started</a>
+</main>
+
+<style global lang="postcss">
+  html,
+  body {
+    @apply h-full;
+  }
+</style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,41 @@ const config = {
   purge: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ['Comfortaa', 'cursive'],
+      body: ['"Open Sans"', 'sans-serif'],
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+
+      n0: '#ffffff',
+      n50: '#f7f7f8',
+      n100: '#eff0f0',
+      n300: '#c8c9cb',
+      n500: '#8e9195',
+      n700: '#545759',
+      n900: '#252627',
+      n1000: '#121212',
+
+      b100: '#c9e0f8',
+      b300: '#73b0ed',
+      b500: '#3c91e6',
+      b600: '#1a70c7',
+      b900: '#0e3d6c',
+
+      g100: '#9ef5c4',
+      g300: '#15cb64',
+      g500: '#11a652',
+      g700: '#0a6130',
+      g900: '#084a24',
+
+      r100: '#fdd4d3',
+      r300: '#fb8884',
+      r500: '#f9564f',
+      r600: '#e20f08',
+      r900: '#8f0a05',
+    },
   },
 
   plugins: [],
